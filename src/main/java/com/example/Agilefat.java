c564e840ea34d97a733c1f89399af053e6948a41
bara-crypto <balac6796@gmail.com>
2026-04-13 10:50:57 +0530

    new one

--- src/main/java/com/example/Agilefat.java
+++ src/main/java/com/example/Agilefat.java
@@ -1,12 +1,48 @@
 package com.example;
 
 public class Agilefat {
+    static public String str1 = "M. adhavan-by adihesh";
+    static public String str2 = "life-by ranveer";
+    static public String str3 = "man-by gauresh";
+
     public static void main(String[] args) {
-        System.out.println("Agilefat Calculator is running!");
-        Agilefat calc = new Agilefat();
-        System.out.println("Result of 5 + 3: " + calc.add(5, 3));
+
+        String result_1 = listingLogic();
+        System.out.println("Listing available books: \n");
+        System.out.println(result_1);
+
+        String result_2 = lending("M. adhavan-by adihesh");
+        System.out.println("Lending: \n");
+        System.out.println(result_2);
+
     }
 
+    static public String listingLogic()
+    {
+        String result = str1+str2+str3;
+        return result;
+    }
+
+    static public String lending(String book)
+    {
+        if (book == str1) 
+        {
+            return (book+"-lended");    
+        }
+        else if (book == str2) 
+        {
+            return(book+"-lended");    
+        }
+        else if (book == str3) 
+        {
+            return(book+"-lended");    
+        }
+        else
+        {
+            return("none");
+        }
+    }
+    
     public int add(int a, int b) {
         return a + b;
     }
