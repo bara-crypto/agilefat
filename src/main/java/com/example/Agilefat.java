c7f0fa83a218c121e60322b53233fc917e1e8e8e
bara-crypto <balac6796@gmail.com>
2026-04-13 08:32:03 +0530

    Add main method to Agilefat class for testing

--- src/main/java/com/example/Agilefat.java
+++ src/main/java/com/example/Agilefat.java
@@ -1,6 +1,12 @@
 package com.example;
 
 public class Agilefat {
+    public static void main(String[] args) {
+        System.out.println("Agilefat Calculator is running!");
+        Agilefat calc = new Agilefat();
+        System.out.println("Result of 5 + 3: " + calc.add(5, 3));
+    }
+
     public int add(int a, int b) {
         return a + b;
     }
