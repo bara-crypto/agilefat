b874146c5591ccc2fa8230d158c214049ada3720
bara-crypto <balac6796@gmail.com>
2026-04-13 10:53:07 +0530

    Remove test cases from AgilefatTest
    Removed test methods for the Agilefat class.

--- src/test/java/com/example/AgilefatTest.java
+++ src/test/java/com/example/AgilefatTest.java
@@ -5,21 +5,4 @@ import org.junit.jupiter.api.Test;
 
 public class Agilefattest {
 
-    Agilefat calculator = new Agilefat();
-
-    @Test
-    void testlending1()
-    {
-        assertEquals("man-by gauresh-lended", calculator.lending("man-by gauresh"));
-    }
-    @Test
-    void testlending2()
-    {
-        assertEquals("life-by ranveer-lended", calculator.lending("life-by ranveer"));
-    }
-    @Test
-    void testlending3()
-    {
-        assertEquals("M. adhavan-by adihesh-lended", calculator.lending("M. adhavan-by adihesh"));
-    }
 }
