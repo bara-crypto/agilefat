a14858f50e6500abe3397dace5eed98f50a4cedc
bara-crypto <balac6796@gmail.com>
2026-04-13 10:57:39 +0530

    nw

--- src/test/java/com/example/AgilefatTest.java
+++ src/test/java/com/example/AgilefatTest.java
@@ -3,6 +3,6 @@ package com.example;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-public class Agilefattest {
+public class AgilefatTest {
 
 }
